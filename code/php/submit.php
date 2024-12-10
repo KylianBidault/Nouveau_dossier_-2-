f3f12af730487e0d405218146aaf5ba87f1d248b
Kylian <Kylian.BIDAULT@etu.uca.fr>
2024-12-10 13:50:24 +0100

    correction hierarchie du site

--- code/php/submit.php
+++ code/php/submit.php
@@ -1,45 +1,37 @@
-<?php
-$reponse = '';
-if(isset($_POST["révolution"])){
-    $reponse="révolution";
-} /*iL vérifie si la variable révolution existe et si elle est pas null*/
-if(isset($_POST["mondiale"])){
-    $reponse="guerre mondiale";
-} /*iL vérifie si la variable mondiale existe et si elle est pas null*/
-if(isset($_POST["froide"])){
-    $reponse="guerre froide";
-} /*iL vérifie si la variable froide existe et si elle est pas null*/
-echo $reponse
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/style.css">
     <title>ÉPOQUE CONTEMPORAINE</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="icon" href="../image/logo-favicon.jpg" type="image/jpg">
 </head>
 <body>
     <section class="article">
         <div class="text">
-            <h1 id="texth1">
-                <p>Merci d'avoir répondu à la question. vous avez mis <?php
-$reponse = '';
-if(isset($_POST["Révolution"])){
-    $reponse="révolution";
-} /*iL vérifie si la variable révolution existe et si elle est pas null*/
-if(isset($_POST["mondiale"])){
-    $reponse="guerre mondiale";
-} /*iL vérifie si la variable mondiale existe et si elle est pas null*/
-if(isset($_POST["froide"])){
-    $reponse="guerre froide";
-} /*iL vérifie si la variable froide existe et si elle est pas null*/
-echo $reponse
-?></p>
-            </h1><!--Phrase de la fin du formulaire PHP-->
+            <h1 id="texth1">Merci d'avoir répondu à la question.</h1>
+            <p>Vous avez sélectionné : 
+                <?php
+                if (isset($_POST["periode"])) {
+                    $reponse = $_POST["periode"]; 
+                    $periode = "";
+                    if ($reponse == "Revolution") {
+                        $periode = "La Révolution";
+                    } elseif ($reponse == "Mondiale") {
+                        $periode = "Les Guerres Mondiales";
+                    } elseif ($reponse == "Froide") {
+                        $periode = "La Guerre Froide";
+                    } 
+                    echo $periode;
+
+                    $fichier = "reponse.txt";
+                    $ligne = "$periode\n";
+                    file_put_contents($fichier, $ligne, FILE_APPEND);
+                } 
+                ?>
+            </p>
         </div>
     </section>
-    
 </body>
 </html>
